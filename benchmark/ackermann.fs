b906049757b9fdd161f5081e86da6762d86820c7
Bach Le <bach@bullno1.com>
2018-05-29 17:41:38 +0800

    Fix ackermann example

--- benchmark/ackermann.fs
+++ benchmark/ackermann.fs
@@ -1,8 +1,8 @@
 : ackermann ( m n -- a )
-    over =0 if nip +1 else
-    dup =0  if drop -1 1 recurse else
-               -1 over ( m n-1 m )
-               -1 -rot ( m-1 m n-1 )
+    over 0= if nip 1+ else
+    dup 0=  if drop 1- 1 recurse else
+               1- over ( m n-1 m )
+               1- -rot ( m-1 m n-1 )
                recurse recurse
     \ " wat" .  .s
     then then ;
